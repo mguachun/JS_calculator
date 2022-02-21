9c5bea1dff7d919668193d4eec60d14cb129d651
Melissa Guachun <1212malka@gmail.com>
2022-02-21 17:44:54 -0500

    setting up constructor and functions needed

--- script.js
+++ script.js
@@ -2,6 +2,38 @@ const numberButton = document.querySelectorAll('[data-number]')
 const operationButton = document.querySelectorAll('[data-operation]')
 const equalsButton =  document.querySelector('[data-equals]')
 const deleteButton =  document.querySelector('[data-delete]')
-const allClearButton =  document.querySelector('[data-equals]')
+const allClearButton =  document.querySelector('[data-all-clear]')
 const previousOperandTextElement =  document.querySelector('[data-previous-operand')
 const currentOperandTextElement =  document.querySelector('[data-current-operand]')
+
+class Calculator {
+    // where to place display prev and current text el
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+
+    clear() {
+
+    }
+
+    delete(){
+
+    }
+
+    appendNumber(number){
+
+    }
+
+    chooseOperation(operation){
+
+    }
+
+    compute(){
+
+    }
+
+    updateDislpay(){
+        
+    }
+}
