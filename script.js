cd9883011b279d772b4f9bd3fcc1d12062ae8ce5
Melissa Guachun <1212malka@gmail.com>
2022-02-25 13:47:45 -0500

    initialize clear method

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+class Calculator {
+    // where to place display prev and current text el
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+
+    }
+
+    delete(){
+
+    }
+
+    appendNumber(number){
+        if (number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+
+    }
+
+    chooseOperation(operation){
+
+    }
+
+    compute(){
+
+    }
+
+    updateDislpay(){
+    this.currentOperandTextElement.innerText = this.currentOperand
+    }
+}
+
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton =  document.querySelector('[data-equals]')
@@ -38,41 +76,3 @@ deleteButton.addEventListener('click', button => {
     calculator.clear()
     calculator.updateDislpay
 })
-
-class Calculator {
-    // where to place display prev and current text el
-    constructor(previousOperandTextElement, currentOperandTextElement){
-        this.previousOperandTextElement = previousOperandTextElement
-        this.currentOperandTextElement = currentOperandTextElement
-        this.clear
-    }
-
-    clear() {
-        this.currentOperand = ''
-        this.previousOperand = ''
-        this.operation = undefined
-
-    }
-
-    delete(){
-
-    }
-
-    appendNumber(number){
-        if (number === '.' && this.currentOperand.includes('.')) return
-        this.currentOperand = this.currentOperand.toString() + number.toString()
-
-    }
-
-    chooseOperation(operation){
-
-    }
-
-    compute(){
-
-    }
-
-    updateDislpay(){
-    this.currentOperandTextElement.innerText = this.currentOperand
-    }
-}
